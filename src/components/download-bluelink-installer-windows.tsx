20163e7a963c06577aa3246c4cfe45dd09600111
Andre Sutherland <andrelsf95@gmail.com>
2025-12-06 15:11:57 +0000

    update windows installer docs

--- src/components/download-bluelink-installer-windows.tsx
+++ src/components/download-bluelink-installer-windows.tsx
@@ -2,23 +2,9 @@ import { cn } from "@/lib/cn";
 import { buttonVariants } from "./ui/button";
 
 export default function DownloadBluelinkInstallerWindows() {
-    const download32bitUrl = process.env.NEXT_PUBLIC_DOWNLOAD_BLUELINK_INSTALLER_WINDOWS_32BIT_URL;
     const download64bitUrl = process.env.NEXT_PUBLIC_DOWNLOAD_BLUELINK_INSTALLER_WINDOWS_64BIT_URL;
     return (
         <div className="flex flex-row gap-4 not-prose">
-            <a
-                href={download32bitUrl}
-                className={cn(
-                    buttonVariants({
-                        color: 'primary',
-                        size: 'sm',
-                        className: 'gap-2 px-3 py-2',
-                    }),
-                )}
-                target="_blank"
-            >
-                Download installer for Windows (32-bit)
-            </a>
             <a
                 href={download64bitUrl}
                 className={cn(
